990980211138a3f8e971142314a5893be2b8f27c
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2019-06-05 12:30:19 +0200

    Fix root node deprecation

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -12,7 +12,13 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('supercache');
-        $rootNode = $treeBuilder->root('supercache');
+
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('supercache');
+        }
 
         $rootNode
             ->children()
